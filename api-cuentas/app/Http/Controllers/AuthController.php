3f36fedf0bb685e2644935e6dc130e1605ac3e9a
Javi17-gib <luisvalverde1330@gmail.com>
2025-11-07 18:02:56 -0600

    JWT

---
+++ api-cuentas/app/Http/Controllers/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTExceptions;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request){
+        $credentials = $request->only("email","password");
+        try{
+            if(! $access_token = JWTAuth::attempt($credentials)){
+                return response ()
+                ->json(["error" => "Credenciales Invalidas"]);
+            }
+            $user = Auth::user();
+            return response()->json([
+                "token"=>$access_token,
+                "user"=>$user
+            ]);
+        }catch(JWTExeption $e){
+            return response()->json([
+                "error"=>"Credenciales invalidas"
+            ]);
+        }
+    }
+}
--- api-cuentas/app/Models/User.php
+++ api-cuentas/app/Models/User.php
@@ -5,8 +5,9 @@ namespace App\Models;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
 
@@ -30,5 +31,19 @@ class User extends Authenticatable
 
     public function transactions() {
         return $this->hasMany(Transaction::class);
+    }
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
